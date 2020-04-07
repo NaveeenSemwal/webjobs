12de862fe1f64cd38f9f06f57900fa9a76bb3485
NaveeenSemwal <semwal.dotnet@gmail.com>
2020-04-07 22:03:54 +0530

    Added Application Insight in EmployeeWebJob

--- EmployeeWebJob/Program.cs
+++ EmployeeWebJob/Program.cs
@@ -27,7 +27,18 @@ namespace EmployeeWebJob
             {
                 b.AddAzureStorageCoreServices();
                 b.AddAzureStorage();
-            });
+            })
+             .ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+
+                // If the key exists in settings, use it to enable Application Insights.
+                string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                if (!string.IsNullOrEmpty(instrumentationKey))
+                {
+                    b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+                }
+            }); ;
             var host = builder.Build();
             using (host)
             {
